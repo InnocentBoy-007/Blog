323bbabe912ff5a08a38ec6cd055abf8296c01d2
Khuraijam Gunindro <65593707+KhGunindro@users.noreply.github.com>
2025-02-14 20:46:35 +0530

    starting with query

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 # instantiate the FastAPI class
 app = FastAPI()
 
-@app.get("/") # / is the base path in FastAPI and .get() is the operation
-def read_root(): # The name does't matter , This the path operation function
-    return {'data': {
-        'name': 'John Doe',
-    }}
+@app.get("/blog")
+def index():
+    return {'data':'blog-list'}
 
-@app.get('/about')
-def about():
-    return {'data': 'about page'}
+# Take care of dynamic routing bugs
+@app.get('/blog/unpublished')
+def unpulished():
+    return {'data':'all unpublished blogs'}
+
+@app.get("/blog/{id}")
+def show(id : int): # defining the type for the parameter
+    return {'data':id}
+
+@app.get("/blog/{id}/comments")
+def comments(id : int):
+    return {'data': {'1','2'}}
 
