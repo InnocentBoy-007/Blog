6c57110578b31de2ef86b1c9681cddac7d78c0f0
Khuraijam Gunindro <65593707+KhGunindro@users.noreply.github.com>
2025-02-15 20:27:01 +0530

    starting request body

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from typing import Optional
 
 # instantiate the FastAPI class
 app = FastAPI()
 
 @app.get("/blog")
-def index():
-    return {'data':'blog-list'}
-
+def index(limit=10,published: bool= True, sort: Optional[str] = None): # Query parameters, we should specify the query parameter type
+    if published:
+        return {'data':f'{limit} blogs from the database'}
+    else:
+        return {'data':f'{limit} unpublished blogs from the database'}
+        
 # Take care of dynamic routing bugs
 @app.get('/blog/unpublished')
 def unpulished():
     return {'data':'all unpublished blogs'}
 
-@app.get("/blog/{id}")
+@app.get("/blog/{id}") # path parameter
 def show(id : int): # defining the type for the parameter
     return {'data':id}
 
 @app.get("/blog/{id}/comments")
-def comments(id : int):
-    return {'data': {'1','2'}}
+def comments(id : int, limit=10):
+    return limit
+    
 
