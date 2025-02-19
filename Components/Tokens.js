14c4f8b7c217380be8da82e4ef5212d58e4f0c60
Innocent Boy <mechanshil7@gmail.com>
2025-02-19 23:57:26 +0530

    Successfully implemented authenticating and registering admin accounts

--- Components/Tokens.js
+++ Components/Tokens.js
@@ -1,8 +1,16 @@
 import jwt from 'jsonwebtoken'
 
+/**
+ * To generate token: tokens.generateToken(payload); ---- 'payload' is 'userId'
+ * To compare token: tokens.compareToken(token); ---- 'token' contains the userId and the secretkey
+ */
 class Tokens {
     async generateToken(payload) {
         return jwt.sign(payload, process.env.SECRETKEY, {'expiresIn':'1h'}); // store the jwt in a cookie inside the browser
+    }
+
+    async compareToken(token) {
+        return jwt.verify(token, process.env.SECRETKEY);
     }
 }
 
--- Components/model.js
+++ Components/model.js
@@ -1,25 +1,9 @@
 import mongoose from "mongoose";
-import bcrypt from 'bcrypt';
 
 // schema
 const adminSchema = new mongoose.Schema({
-    password: { type: String, required: true }
+    email: { type: String, required: true },
+    password: { type: String, required: true, select: false }
 });
 
-// Create model first
-const AdminSchema = mongoose.model('blogadmins', adminSchema);
-
-// creating a hash password for the admins
-const createPassword_admin = async (password) => {
-    try {
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const admin = new AdminSchema({ password: hashedPassword });
-
-        // Save the admin or return the created admin
-        await admin.save();
-    } catch (error) {
-        console.error('Error creating admin:', error);
-    }
-};
-
-export { createPassword_admin, AdminSchema }; // createPassword to be called right after the mongodb connection is successfull
+export default mongoose.model("blogadmins", adminSchema);
